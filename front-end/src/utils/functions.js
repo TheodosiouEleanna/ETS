3c65731e3dceea08d23d47037d7e081abab70e52
Eleanna Theodosiou <ETheodosiou@maseurope.com>
2023-08-10 17:23:50 +0300

    temp commit

---
+++ front-end/src/utils/functions.js
@@ -0,0 +1,41 @@
+export const base64ToBlob = (base64, type = "") => {
+  const byteCharacters = atob(base64.split(",")[1]);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  return new Blob([byteArray], { type: type });
+};
+
+export const getFileFromStorage = () => {
+  const base64 = localStorage.getItem("pdfFile");
+  if (base64) {
+    const retrievedPdfBlob = base64ToBlob(base64, "application/pdf");
+    return retrievedPdfBlob;
+  }
+  return { size: 0 };
+};
+
+export const blobToBase64 = (blob) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = function () {
+      resolve(reader.result);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(blob);
+  });
+};
+
+export const debounce = (fn, delay) => {
+  let timeout;
+  return function (...args) {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+};
